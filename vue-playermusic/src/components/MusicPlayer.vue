0824b8424666416e1690f713cbaaa00c8d6fe565
SamanthaRagnoli <sami.ragnoli@gmail.com>
2024-12-23 11:15:15 +0100

    assets to pubblic

--- vue-playermusic/src/components/MusicPlayer.vue
+++ vue-playermusic/src/components/MusicPlayer.vue
@@ -19,30 +19,30 @@
   
         <div class="controlli">
           <div class="volume-controls">
-            <img class="icon" src="/src/assets/icons/volume-off.svg" alt="Icona volume disattivato"> 
+            <img class="icon" src="/assets/icons/volume-off.svg" alt="Icona volume disattivato"> 
             <div class="volume-bar" @click="setVolume" @mousedown="startDragging" @mouseup="stopDragging" @mousemove="dragVolume">
               <div class="volume-level" :style="{ width: volumeLevel + '%' }"></div>
             </div>
-            <img class="icon" src="/src/assets/icons/volume-up.svg" alt="Icona volume attivo">
+            <img class="icon" src="/assets/icons/volume-up.svg" alt="Icona volume attivo">
           </div>
           <div class="playback-controls">
             <button class="prev" @click="prev">
-              <img class="pcontrol" src="/src/assets/icons/backward.svg" alt="Indietro"> 
+              <img class="pcontrol" src="/assets/icons/backward.svg" alt="Indietro"> 
             </button>
             <button class="play" v-if="!isPlaying" @click="play">
-              <img class="pcontrol" src="/src/assets/icons/play.svg" alt="Play">
+              <img class="pcontrol" src="/assets/icons/play.svg" alt="Play">
             </button>
             <button class="pause" v-else @click="pause">
-              <img class="pcontrol" src="/src/assets/icons/pause.svg" alt="Pausa">
+              <img class="pcontrol" src="/assets/icons/pause.svg" alt="Pausa">
             </button>
             <button class="next" @click="next">
-              <img class="pcontrol" src="/src/assets/icons/forward.svg" alt="Avanti">
+              <img class="pcontrol" src="/assets/icons/forward.svg" alt="Avanti">
             </button> 
           </div>
           <button class="repeat-button" @click="toggleRepeat" :class="{ active: repeat, repeatOne: repeat === 'one' }">
-            <img v-if="repeat === 'loop'" class="icon-on" src="/src/assets/icons/repeat.svg" alt="Ripeti in loop">
-            <img v-else-if="repeat === 'one'" class="icon-on" src="/src/assets/icons/repeat-1.svg" alt="Ripeti una volta">
-            <img v-else class="icon" src="/src/assets/icons/repeat.svg" alt="Ripetizione disattivata"> 
+            <img v-if="repeat === 'loop'" class="icon-on" src="/assets/icons/repeat.svg" alt="Ripeti in loop">
+            <img v-else-if="repeat === 'one'" class="icon-on" src="/assets/icons/repeat-1.svg" alt="Ripeti una volta">
+            <img v-else class="icon" src="/assets/icons/repeat.svg" alt="Ripetizione disattivata"> 
           </button>
           </div>
         </div>
@@ -72,176 +72,176 @@
         {
           title: 'Ectoplasm',
           artist: 'Boom',
-          src: '/src/assets/Ectoplasm.mp3',
-          img: '/src/assets/Images/img_prova.jpg' 
+          src: '/assets/Music/Ectoplasm.mp3',
+          img: '/assets/Images/img_prova.jpg' 
         },
         {
           title: 'GroovyBaby',
           artist: 'Grace',
-          src: '/src/assets/GroovyBaby.mp3',
-          img: '/src/assets/Images/img_prova2.jpg' 
+          src: '/assets/Music/GroovyBaby.mp3',
+          img: '/assets/Images/img_prova2.jpg' 
         },
         {
           title: 'Fly Me To The Moon',
           artist: 'Frank Sinatra',
-          src: '/src/assets/Fly Me To The Moon (2008 Remastered).mp3',
-          img: '/src/assets/Images/frank_sinatra.jpg' 
+          src: '/assets/Music/Fly Me To The Moon (2008 Remastered).mp3',
+          img: '/assets/Images/frank_sinatra.jpg' 
         },
         {
           title: 'Estate',
           artist: 'Bruno Martino',
-          src: '/src/assets/Estate.mp3',
-          img: '/src/assets/Images/bruno_martino.jpg' 
+          src: '/assets/Music/Estate.mp3',
+          img: '/assets/Images/bruno_martino.jpg' 
         },
         {
           title: 'Sweet Child O Mine',
           artist: 'Guns N Roses',
-          src: '/src/assets/Guns N Roses - Sweet Child O Mine (Official Music Video) .mp3',
-          img: '/src/assets/Images/gun_n_roses.jpg' 
+          src: '/assets/Music/Guns N Roses - Sweet Child O Mine (Official Music Video) .mp3',
+          img: '/assets/Images/gun_n_roses.jpg' 
         },
         {
           title: 'Il Ragazzo Della Via Gluck',
           artist: 'Adriano Celentano',
-          src: '/src/assets/Il Ragazzo Della Via Gluck.mp3',
-          img: '/src/assets/Images/adriano_celentano.jpg' 
+          src: '/assets/Music/Il Ragazzo Della Via Gluck.mp3',
+          img: '/assets/Images/adriano_celentano.jpg' 
         },
         {
           title: 'Billie Jean',
           artist: 'Michael Jackson',
-          src: '/src/assets/Michael Jackson - Billie Jean (Official Video).mp3',
-          img: '/src/assets/Images/michael_jackson.jpg' 
+          src: '/assets/Music/Michael Jackson - Billie Jean (Official Video).mp3',
+          img: '/assets/Images/michael_jackson.jpg' 
         },
         {
           title: 'Se Telefonando',
           artist: 'Mina',
-          src: '/src/assets/Mina - Se telefonando (1966).mp3',
-          img: '/src/assets/Images/mina.jpg' 
+          src: '/assets/Music/Mina - Se telefonando (1966).mp3',
+          img: '/assets/Images/mina.jpg' 
         },
         {
           title: 'Bohemian Rapsody',
           artist: 'Queen',
-          src: '/src/assets/Queen – Bohemian Rhapsody (Official Video Remastered).mp3',
-          img: '/src/assets/Images/queen.jpg' 
+          src: '/assets/Music/Queen – Bohemian Rhapsody (Official Video Remastered).mp3',
+          img: '/assets/Images/queen.jpg' 
         },
         {
           title: 'A Mano A Mano',
           artist: 'Rino Gaetano',
-          src: '/src/assets/Rino Gaetano - A mano a mano (Live).mp3',
-          img: '/src/assets/Images/rino_gaetano.jpg' 
+          src: '/assets/Music/Rino Gaetano - A mano a mano (Live).mp3',
+          img: '/assets/Images/rino_gaetano.jpg' 
         },
         {
           title: 'APT',
           artist: 'Rosé & Bruno Mars',
-          src: '/src/assets/ROSÉ & Bruno Mars - APT. (Official Music Video).mp3',
-          img: '/src/assets/Images/rosé_bruno_mars.jpg' 
+          src: '/assets/Music/ROSÉ & Bruno Mars - APT. (Official Music Video).mp3',
+          img: '/assets/Images/rosé_bruno_mars.jpg' 
         },
         {
           title: 'Houdini',
           artist: 'Eminem',
-          src: '/src/assets/Houdini.mp3',
-          img: '/src/assets/Images/eminem.jpg' 
+          src: '/assets/Music/Houdini.mp3',
+          img: '/assets/Images/eminem.jpg' 
         },
         {
           title: 'Work',
           artist: 'Rihanna',
-          src: '/src/assets/Work.mp3',
-          img: '/src/assets/Images/rihanna.jpg' 
+          src: '/assets/Music/Work.mp3',
+          img: '/assets/Images/rihanna.jpg' 
         },
         {
           title: 'Morire X Te',
           artist: 'Mida',
-          src: '/src/assets/Mida - Morire x te (Official Video).mp3',
-          img: '/src/assets/Images/mida.jpg' 
+          src: '/assets/Music/Mida - Morire x te (Official Video).mp3',
+          img: '/assets/Images/mida.jpg' 
         },
         {
           title: 'Espresso',
           artist: 'Sabrina Carpenter',
-          src: '/src/assets/Sabrina Carpenter - Espresso (Official Audio).mp3',
-          img: '/src/assets/Images/sabrina_carpenter.jpg' 
+          src: '/assets/Music/Sabrina Carpenter - Espresso (Official Audio).mp3',
+          img: '/assets/Images/sabrina_carpenter.jpg' 
         },
         {
           title: 'Per Due Come Noi',
           artist: 'Olly, Angelina Mango',
-          src: '/src/assets/Olly, Angelina Mango, JVLI - Per due come noi (Official Video).mp3',
-          img: '/src/assets/Images/olly_mango.jpg' 
+          src: '/assets/Music/Olly, Angelina Mango, JVLI - Per due come noi (Official Video).mp3',
+          img: '/assets/Images/olly_mango.jpg' 
         },
         {
           title: 'Die With A Smile',
           artist: 'Lady Gaga, Bruno Mars',
-          src: '/src/assets/Lady Gaga, Bruno Mars - Die With A Smile (Official Music Video).mp3',
-          img: '/src/assets/Images/gaga_mars.jpg' 
+          src: '/assets/Music/Lady Gaga, Bruno Mars - Die With A Smile (Official Music Video).mp3',
+          img: '/assets/Images/gaga_mars.jpg' 
         },
         {
           title: 'Il Filo Rosso',
           artist: 'Alfa',
-          src: '/src/assets/ALFA - il filo rosso.mp3',
-          img: '/src/assets/Images/alfa.jpg' 
+          src: '/assets/Music/ALFA - il filo rosso.mp3',
+          img: '/assets/Images/alfa.jpg' 
         },
         {
           title: 'Birds Of Feather',
           artist: 'Billie Eilish',
-          src: '/src/assets/Billie Eilish - BIRDS OF A FEATHER (Official Music Video).mp3',
-          img: '/src/assets/Images/billie_eilish.jpg' 
+          src: '/assets/Music/Billie Eilish - BIRDS OF A FEATHER (Official Music Video).mp3',
+          img: '/assets/Images/billie_eilish.jpg' 
         },
         {
           title: 'Grandine',
           artist: 'Holden',
-          src: '/src/assets/Holden - GRANDINE feat. mew (Visual Video).mp3',
-          img: '/src/assets/Images/holden.jpg' 
+          src: '/assets/Music/Holden - GRANDINE feat. mew (Visual Video).mp3',
+          img: '/assets/Images/holden.jpg' 
         },
         {
           title: 'Rosso Relativo',
           artist: 'Tiziano Ferro',
-          src: '/src/assets/Tiziano Ferro - Rosso Relativo (Official Video).mp3',
-          img: '/src/assets/Images/tiziano_ferro.jpg' 
+          src: '/assets/Music/Tiziano Ferro - Rosso Relativo (Official Video).mp3',
+          img: '/assets/Images/tiziano_ferro.jpg' 
         },
         {
           title: 'Unforgivable Sinner',
           artist: 'Lene Marlin',
-          src: '/src/assets/Lene Marlin - Unforgivable Sinner.mp3',
-          img: '/src/assets/Images/lene_marlin.jpg' 
+          src: '/assets/Music/Lene Marlin - Unforgivable Sinner.mp3',
+          img: '/assets/Images/lene_marlin.jpg' 
         },
         {
           title: 'Maybe Tomorrow',
           artist: 'Stereophonics',
-          src: '/src/assets/Maybe Tomorrow.mp3',
-          img: '/src/assets/Images/stereophonics.jpg' 
+          src: '/assets/Music/Maybe Tomorrow.mp3',
+          img: '/assets/Images/stereophonics.jpg' 
         },   
         {
           title: 'We Pray',
           artist: 'Coldplay, Little Simz, Burna Boy, Elyanna, TINI',
-          src: '/src/assets/WE PRAY - Coldplay, Little Simz, Burna Boy, Elyanna, TINI (Official Audio).mp3',
-          img: '/src/assets/Images/coldplay_others.jpg' 
+          src: '/assets/Music/WE PRAY - Coldplay, Little Simz, Burna Boy, Elyanna, TINI (Official Audio).mp3',
+          img: '/assets/Images/coldplay_others.jpg' 
         },
         {
           title: 'They Dont Care About Us',
           artist: 'Michael Jackson',
-          src: '/src/assets/They Dont Care About Us (Remastered Version).mp3',
-          img: '/src/assets/Images/michael_jackson_2.jpg' 
+          src: '/assets/Music/They Dont Care About Us (Remastered Version).mp3',
+          img: '/assets/Images/michael_jackson_2.jpg' 
         },
         {
           title: 'Mezzocielo',
           artist: 'Ilan',
-          src: '/src/assets/Mezzocielo.mp3',
-          img: '/src/assets/Images/ilan.jpg' 
+          src: '/assets/Music/Mezzocielo.mp3',
+          img: '/assets/Images/ilan.jpg' 
         },
         {
           title: 'Perché',
           artist: 'Chiamamifaro',
-          src: '/src/assets/Perché.mp3',
-          img: '/src/assets/Images/chiamamifaro.jpg' 
+          src: '/assets/Music/Perché.mp3',
+          img: '/assets/Images/chiamamifaro.jpg' 
         },
         {
           title: 'Madrid',
           artist: 'Senza Cri',
-          src: '/src/assets/MADRID.mp3',
-          img: '/src/assets/Images/senza_cri.jpg' 
+          src: '/assets/Music/MADRID.mp3',
+          img: '/assets/Images/senza_cri.jpg' 
         },
         {
           title: 'Lose Yourself',
           artist: 'Eminem',
-          src: '/src/assets/Eminem - Lose Yourself.mp3',
-          img: '/src/assets/Images/eminem_2.jpg' 
+          src: '/assets/Music/Eminem - Lose Yourself.mp3',
+          img: '/assets/Images/eminem_2.jpg' 
         },
 
         
